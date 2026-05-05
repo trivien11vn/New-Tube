8d009599b8d08e984e2c1366762596082cdce45b
trivien11vn <triviencoderr@gmail.com>
2026-05-05 17:02:27 +0700

    update TRPC configuration

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,7 +5,7 @@ import { ErrorBoundary } from "react-error-boundary";
 
 export default async function Home() {
   void trpc.hello.prefetch({
-    text: "Tri Viennn"
+    text: "NHTV"
   })
 
   return (
