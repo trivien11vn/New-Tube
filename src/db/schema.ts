6d3b6001caab424107a52a161bfab38dceefce8f
trivien11vn <triviencoderr@gmail.com>
2026-05-06 17:59:57 +0700

    update video schema 5:34:50 -1

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,6 @@
 import { pgTable, text, timestamp, uniqueIndex, uuid } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+
 
 export const users = pgTable("users", {
     id: uuid("id").primaryKey().defaultRandom(),
@@ -17,3 +19,37 @@ export const categories = pgTable("categories", {
     createdAt: timestamp("created_at").defaultNow().notNull(),
     updatedAt: timestamp("updated_at").defaultNow().notNull()
 }, (t) => [uniqueIndex("name_idx").on(t.name)])
+
+
+export const videos = pgTable("videos", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    title: text("title").notNull(),
+    description: text("description"),
+    userId: uuid("user_id").references(() => users.id, {
+        onDelete: "cascade"
+    }).notNull(),
+    categoryId: uuid("category_id").references(() => categories.id, {
+        onDelete: "set null"
+    }),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull()
+});
+
+export const userRelations = relations(users, ({ many }) => ({
+    videos: many(videos)
+}))
+
+export const videoRelations = relations(videos, ({ one }) => ({
+    user: one(users, {
+        fields: [videos.userId],
+        references: [users.id]
+    }),
+    category: one(categories, {
+        fields: [videos.categoryId],
+        references: [categories.id]
+    })
+}))
+
+export const categoryRelations = relations(categories, ({ many }) => ({
+    videos: many(videos)
+}))
--- src/modules/categories/server/procedures.ts
+++ src/modules/categories/server/procedures.ts
@@ -1,7 +1,6 @@
 import { db } from "@/db";
 import { categories } from "@/db/schema";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
-import { TRPCError } from "@trpc/server";
 
 export const categoriesRouter = createTRPCRouter({
     getMany: baseProcedure.query(async () => {
