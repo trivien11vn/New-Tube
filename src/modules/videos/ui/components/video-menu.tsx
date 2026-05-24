6865d7a3e31795c2eefc3f7ac74977eee864bb6e
trivien11vn <triviencoderr@gmail.com>
2026-05-24 22:37:18 +0700

    update Video Page 1:03:30 -2

---
+++ src/modules/videos/ui/components/video-menu.tsx
@@ -0,0 +1,53 @@
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Button } from "@/components/ui/button";
+import { ListPlusIcon, MoreVerticalIcon, ShareIcon, Trash2Icon } from "lucide-react";
+import { toast } from "sonner";
+
+interface VideoMenuProps {
+    videoId: string;
+    variant?: "ghost" | "secondary";
+    onRemove?: () => void;
+}
+
+// TODO: implement whats left
+export const VideoMenu = ({ videoId, variant, onRemove }: VideoMenuProps) => {
+    const onShare = () => {
+        // TODO: change if deploy outside of vercel
+        const fullUrl = `${process.env.VERCEL_URL || 'http://localhost:3000'}/videos/${videoId}`;
+
+        navigator.clipboard.writeText(fullUrl);
+        toast.success("Link copied to clipboard");
+    }
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant={variant} size="icon" className="rounded-full">
+                    <MoreVerticalIcon />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" onClick={(e) => e.stopPropagation()}>
+                <DropdownMenuItem onClick={onShare}>
+                    <ShareIcon className="mr-2 size-4" />
+                    Share
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => { }}>
+                    <ListPlusIcon className="mr-2 size-4" />
+                    Add to playlist
+                </DropdownMenuItem>
+                {
+                    onRemove && (
+                        <DropdownMenuItem onClick={() => { }}>
+                            <Trash2Icon className="mr-2 size-4" />
+                            Remove
+                        </DropdownMenuItem>
+                    )
+                }
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
