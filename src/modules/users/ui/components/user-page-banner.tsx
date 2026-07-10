735e7135d2dc34904eeb844ebfdf15ef6bf536be
trivien11vn <triviencoderr@gmail.com>
2026-07-10 13:36:58 +0700

    finish banner upload

--- src/modules/users/ui/components/user-page-banner.tsx
+++ src/modules/users/ui/components/user-page-banner.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
 import { cn } from "@/lib/utils";
 import { UserGetOneOutput } from "@/modules/users/types";
+import { BannerUploadModal } from "@/modules/users/ui/components/banner-upload-modal";
 import { useAuth } from "@clerk/nextjs";
 import { Edit2Icon } from "lucide-react";
+import { useState } from "react";
 
 interface UserPageBannerProps {
     user: UserGetOneOutput
@@ -11,9 +15,15 @@ interface UserPageBannerProps {
 
 export const UserPageBanner = ({ user }: UserPageBannerProps) => {
     const { userId } = useAuth();
+    const [isBannerUploadModalOpen, setIsBannerUploadModalOpen] = useState(false)
     return (
         <div className="relative group">
             {/* TODO: Add upload banner modal */}
+            <BannerUploadModal
+                userId={user.id}
+                open={isBannerUploadModalOpen}
+                onOpenChange={setIsBannerUploadModalOpen}
+            />
             <div
                 className={cn("w-full max-h-[200px] h-[15vh] md:h-[25vh] bg-gradient-to-r from-gray-100 to-gray-200 rounded-xl", user.bannerUrl ? "bg-cover bg-center" : "bg-gray-100")}
                 style={{
@@ -26,6 +36,7 @@ export const UserPageBanner = ({ user }: UserPageBannerProps) => {
                             type="button"
                             size="icon"
                             className="absolute top-4 right-4 rounded-full bg-black/50 hover:bg-black/50 opacity-100 md:opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+                            onClick={() => { setIsBannerUploadModalOpen(true) }}
                         >
                             <Edit2Icon className="size-4 text-white" />
                         </Button>
